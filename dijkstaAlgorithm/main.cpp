85447b6548407e9a9b72f343375b601673bf07a9
Robert Torres <torresrobert@me.com>
2017-10-10 13:03:22 -0400

    Initial Commit

---
+++ dijkstaAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dijkstaAlgorithm
+//
+//  Created by Robert Torres on 10/10/17.
+//  Copyright © 2017 Robert Torres. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
